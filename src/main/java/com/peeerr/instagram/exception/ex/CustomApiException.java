b49a712f07a4c638f1ff1f1c259535b060473a31
JunHyeongChoi <ajfeh22@gmail.com>
2023-10-09 20:30:07 +0900

    중복 구독 예외처리 (#6)
    구독한 사람을 또 구독할 경우의 예외처리

---
+++ src/main/java/com/peeerr/instagram/exception/ex/CustomApiException.java
@@ -0,0 +1,9 @@
+package com.peeerr.instagram.exception.ex;
+
+public class CustomApiException extends RuntimeException {
+
+    public CustomApiException(String message) {
+        super(message);
+    }
+
+}
