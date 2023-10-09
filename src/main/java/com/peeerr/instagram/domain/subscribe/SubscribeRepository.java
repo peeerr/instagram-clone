c2072cb7eb0c52b6e26d2ecd377e460fdeb76486
JunHyeongChoi <ajfeh22@gmail.com>
2023-10-09 20:29:37 +0900

    구독 네이티브 쿼리 작성 (#6)
    Subscribe 엔티티는 fromUser이나 toUser 타입이 User이기 때문에 JPA로 save 하는 것보다 네이티브 쿼리로 받은 아이디 그대로 저장하는 것이 더 편함

---
+++ src/main/java/com/peeerr/instagram/domain/subscribe/SubscribeRepository.java
@@ -0,0 +1,18 @@
+package com.peeerr.instagram.domain.subscribe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+
+    @Modifying
+    @Query(value = "INSERT INTO subscribe(from_user_id, to_user_id, create_date) VALUES(:fromUserId, :toUserId, now())", nativeQuery = true)
+    void subscribe(@Param("fromUserId") Long fromUserId, @Param("toUserId") Long toUserId);
+
+    @Modifying
+    @Query(value = "DELETE FROM subscribe WHERE from_user_id = :fromUserId AND to_user_id = :toUserId", nativeQuery = true)
+    void unsubscribe(@Param("fromUserId") Long fromUserId, @Param("toUserId") Long toUserId);
+
+}
