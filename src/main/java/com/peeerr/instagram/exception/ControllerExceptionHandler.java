9af54a184ded1c1b5824a4db2d13f1909fc6ee9a
JunHyeongChoi <ajfeh22@gmail.com>
2023-10-09 16:58:52 +0900

    회원정보 수정 예외처리 (#4)
    ResponseEntity를 사용한 이유는 ajax 요청 시 done과 fail을 구분하여 처리하기 위해서이다.
    
    ResponseEntity로 상태코드를 지정해서 넘겨주지 않으면 상태코드가 200번대라서 오류가 발생해도 done만 처리된다.

--- src/main/java/com/peeerr/instagram/exception/ControllerExceptionHandler.java
+++ src/main/java/com/peeerr/instagram/exception/ControllerExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.peeerr.instagram.exception;
 
+import com.peeerr.instagram.dto.CMResponse;
+import com.peeerr.instagram.exception.ex.CustomValidationApiException;
 import com.peeerr.instagram.exception.ex.CustomValidationException;
 import com.peeerr.instagram.util.Script;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +14,12 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(CustomValidationException.class)
     public String validationException(CustomValidationException e) {
         return Script.back(e.getErrorMap().toString());
+    }
+
+    @ExceptionHandler(CustomValidationApiException.class)
+    public ResponseEntity<CMResponse<?>> validationApiException(CustomValidationApiException e) {
+        return ResponseEntity.badRequest()
+                .body(new CMResponse<>(-1, e.getMessage(), e.getErrorMap()));
     }
 
 }
