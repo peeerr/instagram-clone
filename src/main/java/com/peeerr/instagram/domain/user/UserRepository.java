bce2ea373d710e51e1228fabc6c3af0881f5ac08
JunHyeongChoi <ajfeh22@gmail.com>
2023-10-09 16:52:51 +0900

    쿼리메소드 추가 (#4)

--- src/main/java/com/peeerr/instagram/domain/user/UserRepository.java
+++ src/main/java/com/peeerr/instagram/domain/user/UserRepository.java
@@ -2,5 +2,10 @@ package com.peeerr.instagram.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByUsername(String username);
+
 }
