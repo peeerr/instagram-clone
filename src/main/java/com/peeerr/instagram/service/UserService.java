86b4b8b383d21e4454540adc972e1611a9ea9cde
JunHyeongChoi <ajfeh22@gmail.com>
2023-10-10 22:41:32 +0900

    유저에 따른 구독, 사진 업로드 버튼 처리 (#8)
    프로필 페이지가 자신의 페이지면 사진 업로드 버튼이, 다른 사람의 페이지면 구독 버튼이 뜨게끔 구현함

--- src/main/java/com/peeerr/instagram/service/UserService.java
+++ src/main/java/com/peeerr/instagram/service/UserService.java
@@ -2,7 +2,8 @@ package com.peeerr.instagram.service;
 
 import com.peeerr.instagram.domain.user.User;
 import com.peeerr.instagram.domain.user.UserRepository;
-import com.peeerr.instagram.exception.ex.CustomValidationApiException;
+import com.peeerr.instagram.dto.user.UserProfileRequest;
+import com.peeerr.instagram.exception.ex.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,10 +16,23 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional(readOnly = true)
+    public UserProfileRequest getProfile(Long pageUserId, Long principalId) {
+        UserProfileRequest dto = new UserProfileRequest();
+
+        User user = userRepository.findById(pageUserId).orElseThrow(() -> new CustomException("해당 프로필 페이지는 존재하지 않습니다."));
+
+        dto.setUser(user);
+        dto.setImageCounter(user.getImages().size());
+        dto.setPageOwnerState(pageUserId == principalId);
+
+        return dto;
+    }
+
     @Transactional
     public User update(Long id, User user) {
         // 영속화
-        User userEntity = userRepository.findById(id).orElseThrow(() -> new CustomValidationApiException("해당 유저를 찾을 수 없습니다."));
+        User userEntity = userRepository.findById(id).orElseThrow(() -> new CustomException("해당 유저를 찾을 수 없습니다."));
 
         // 영속화 된 오브젝트 더티체킹
         userEntity.setName(user.getName());
