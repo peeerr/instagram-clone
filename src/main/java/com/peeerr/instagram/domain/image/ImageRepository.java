929e0e109f74995309de95a6721e62dee1feac3c
JunHyeongChoi <ajfeh22@gmail.com>
2023-11-29 21:39:36 +0900

    구독한 유저의 스토리만 불러오는 쿼리 작성 (#12)

--- src/main/java/com/peeerr/instagram/domain/image/ImageRepository.java
+++ src/main/java/com/peeerr/instagram/domain/image/ImageRepository.java
@@ -1,6 +1,13 @@
 package com.peeerr.instagram.domain.image;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    @Query(value = "SELECT * FROM image WHERE user_id IN (SELECT to_user_id FROM subscribe WHERE from_user_id = :principalId) ORDER BY id DESC;", nativeQuery = true)
+    Page<Image> getStories(Long principalId, Pageable pageable);
+
 }
